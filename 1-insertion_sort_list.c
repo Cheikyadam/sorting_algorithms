0e8829c5b7c2fd684272af617b34e75b1f8e12d4
Cheikyadam <urbaingande@gmail.com>
2023-12-19 08:02:55 -0800

    1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,51 +1,50 @@
 #include "sort.h"
 
 /**
- * swap - to swap two nodes
- * @head: the list
- * @node1: the first node
- * @node2: 2nd node
+ * quickswap - to swap two nodes
+ * @tmp: the first node
+ * @to_move: 2nd node
  *
  * Return: Nothing
  */
 
-void swap(listint_t **head, listint_t *node1, listint_t *node2)
+void quickswap(listint_t *tmp, listint_t *to_move)
 {
-	listint_t *tempPrev = NULL, *tempNext = NULL;
+	tmp->next = to_move->next;
+	to_move->next->prev = tmp;
+}
 
-	(void)head;
-	if (node1 == NULL || node2 == NULL || node1 == node2)
-		return;
-	if (node1->prev != NULL)
-	{
-		node1->prev->next = node2;
-	}
-	else
-	{
-		(*head) = node2;
-	}
-	if (node2->prev != NULL)
-	{
-		node2->prev->next = node1;
-	}
-	else
-	{
-		(*head) = node1;
-	}
-	if (node1->next != NULL)
-		node1->next->prev = node2;
+/**
+ * slowswap - to swap two nodes
+ * @tmp: the first node
+ * @to_move: 2nd node
+ *
+ * Return: Nothing
+ */
 
-	if (node2->next != NULL)
-		node2->next->prev = node1;
+void slowswap(listint_t *tmp, listint_t *to_move)
+{
+	to_move->next = tmp->next;
+	to_move->prev = tmp;
+	tmp->next->prev = to_move;
+	tmp->next = to_move;
+}
 
-	tempPrev = node1->prev;
-	tempNext = node1->next;
-
-	node1->prev = node2->prev;
-	node1->next = node2->next;
-
-	node2->prev = tempPrev;
-	node2->next = tempNext;
+/**
+ * initswap - to swap two nodes
+ * @tmp: the first node
+ * @to_move: 2nd node
+ * @list: the list
+ *
+ * Return: Nothing
+ */
+void initswap(listint_t *tmp, listint_t *to_move, listint_t **list)
+{
+	to_move->next = tmp;
+	tmp->prev = to_move;
+	to_move->prev = NULL;
+	to_move->next = tmp;
+	(*list) = to_move;
 }
 
 /**
@@ -59,43 +58,43 @@ void swap(listint_t **head, listint_t *node1, listint_t *node2)
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *tmp = NULL, *mov = (*list), *to_move = NULL;
-	int print = 0;
+	int c = 0;
 
-	if (list == NULL)
-		return;
-	if ((*list) == NULL)
+	if (list == NULL || (*list) == NULL)
 		return;
 	while (mov != NULL)
 	{
-		print = 0;
 		tmp = mov;
+		c = 0;
 		if (tmp->prev != NULL && tmp->prev->n > tmp->n)
 		{
-			print = 1;
 			to_move = tmp;
 			tmp->prev->next = tmp->next;
 			if (tmp->next != NULL)
 				tmp->next->prev = tmp->prev;
 			tmp =  tmp->prev;
 			while (tmp->prev != NULL && to_move->n < tmp->n)
+			{
+				c += 1;
+				if (c > 1)
+					quickswap(tmp, to_move);
 				tmp = tmp->prev;
-			if (to_move->n >= tmp->n)
-			{
-				tmp->next->prev = to_move;
-				to_move->next = tmp->next;
-				tmp->next = to_move;
-				to_move->prev = tmp;
+				slowswap(tmp, to_move);
+				print_list((*list));
 			}
-			else
+			if (tmp->prev == NULL)
 			{
-				tmp->prev = to_move;
-				to_move->prev = NULL;
-				to_move->next = tmp;
-				(*list) = to_move;
+				if (to_move->n >= tmp->n)
+					slowswap(tmp, to_move);
+				else
+				{
+					if (c >= 1)
+						quickswap(tmp, to_move);
+					initswap(tmp, to_move, (list));
+				}
+				print_list((*list));
 			}
 		}
-		if (print == 1)
-			print_list((*list));
 		mov = mov->next;
 	}
 }
